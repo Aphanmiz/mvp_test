c9aa496c90a4ae1d563515198fd848d245cbd1b8
Aphanmiz <99907676+Aphanmiz@users.noreply.github.com>
2023-06-20 11:04:24 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import LandingFrame from "./screens/LandingFrame";
+import Journal from "./screens/Journal";
+import SignUp from "./screens/SignUp";
+import Settings from "./screens/Settings";
+import Actoon from "./screens/Actoon";
+import Museum from "./screens/Museum";
+import Reward from "./screens/Reward";
+import Interests from "./screens/Interests";
+import SignIn from "./screens/SignIn";
 
-export default function App() {
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+
+// Root component for the React Native app
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+  const [fontsLoaded, error] = useFonts({
+    "Open Sans_regular": require("./assets/fonts/Open_Sans_regular.ttf"),
+    "Open Sans_semibold": require("./assets/fonts/Open_Sans_semibold.ttf"),
+    "Open Sans_bold": require("./assets/fonts/Open_Sans_bold.ttf"),
+    Assistant_semibold: require("./assets/fonts/Assistant_semibold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="LandingFrame"
+              component={LandingFrame}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Journal"
+              component={Journal}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Settings"
+              component={Settings}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Actoon"
+              component={Actoon}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Museum"
+              component={Museum}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Reward"
+              component={Reward}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Interests"
+              component={Interests}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="SignIn"
+              component={SignIn}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
---
+++ GlobalStyles.js
@@ -0,0 +1,36 @@
+/* fonts */
+export const FontFamily = {
+  openSansBold: "Open Sans_bold",
+  subhead: "Open Sans_semibold",
+  caption: "Open Sans_regular",
+  assistantSemibold: "Assistant_semibold",
+  jejuHallasanRegular: "JejuHallasan_regular",
+};
+/* font sizes */
+export const FontSize = {
+  subhead_size: 18,
+  size_xl: 20,
+  label_size: 16,
+  caption_size: 13,
+  size_11xl: 30,
+  size_mini: 15,
+  size_6xl: 25,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  dark: "#595959",
+  dimgray_100: "#555",
+  blueviolet: "#8f00ff",
+  dark50: "#acacac",
+  light: "#f2f2f2",
+  black: "#000",
+};
+/* border radiuses */
+export const Border = {
+  br_12xs: 1,
+  br_5xs: 8,
+  br_2xl: 21,
+  br_3xs: 10,
+  br_xs: 12,
+};
